a88cca4a77a7ee02d14341cb32cac4fdeafd3418
System Coding <revanthnagmunagala@gmail.com>
2023-05-28 16:25:02 +0530

    Changed .gitignore

---
+++ build/premake5.lua
@@ -0,0 +1,38 @@
+workspace "OpenGLEngine"
+configurations {"Debug", "Release"}
+platforms {"x64"}
+
+project "OpenGLEngine"
+kind "ConsoleApp"
+language "C++"
+targetdir "bin/%{cfg.buildcfg}"
+
+files {"../src/Application.cpp"}
+files {"../src/engine/**.cpp", "src/include/**.h"}
+files {"../vendor/stb_image/stb_image.cpp"}
+files {"../sandbox/*.cpp"}
+
+includedirs {"include"}
+
+-- OpenGL
+links {"GL"}
+
+-- GLEW
+links {"GLEW"}
+
+-- GLFW
+links {"glfw", "X11", "dl", "pthread"}
+
+-- stb_image
+includedirs {"../vendor/stb_image/include"}
+
+-- spdlog
+includedirs {"../vendor/spdlog/include"}
+
+filter "configurations:Debug"
+defines {"DEBUG"}
+symbols "On"
+
+filter "configurations:Release"
+defines {"NDEBUG"}
+optimize "On"
